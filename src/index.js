b9459df8db995a77cdcadc58de9f2f0b5c50f231
Jonas <jonashalvorsen2013@live.no>
2024-09-24 15:53:45 +0200

    Add-renderCard

--- src/index.js
+++ src/index.js
@@ -4,3 +4,38 @@ console.log(data);
 //You can start simple and just render a single 
 //pokemon card from the first element
 console.log(data[0]);
+
+const cardList = document.querySelector(".cards")
+
+function renderCard(cardData) {
+    const li = document.createElement("li")
+    //li.setAttribute("class", "card")
+    li.className = "card"
+
+    const h2 = document.createElement("h2")
+    //h2.setAttribute("class", "card--title")
+    h2.className = "card--title"
+    h2.textContent = cardData.name.charAt(0).toUpperCase() + cardData.name.slice(1)
+
+    const img = document.createElement("img");
+    img.width = "256"
+    img.className = "card--img"
+    img.src = cardData.sprites.other["official-artwork"].front_default
+    img.alt = cardData.name
+
+    const ul = document.createElement("ul")
+    ul.className = "card--text"
+    const liHp = document.createElement("li")
+    const statFound = cardData.stats.find((item) => item.stat.name === "hp")
+    liHp.textContent = statFound.stat.name + ': ' + statFound.base_stat
+
+    ul.appendChild(liHp)
+
+    li.appendChild(h2)
+    li.appendChild(img)
+    li.appendChild(ul)
+
+    return li
+}
+
+cardList.appendChild(renderCard(data[0]))
